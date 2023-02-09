354d61c9f5aa6b81a61734da0b895aa551f57bcc
AyushGupta2301 <ayush2301ak@gmail.com>
2023-02-09 23:15:08 +0530

    added_expiration

--- data-access/DetaSessionStore.js
+++ data-access/DetaSessionStore.js
@@ -4,60 +4,60 @@
 const Deta = require('deta').Deta;
 const session = require('express-session');
 
-    class DetaSessionStore extends session.Store{
-        constructor(obj){
-            super();
-            this.deta = Deta(obj.projectKey);
-            this.db = this.deta.Base(obj.dbName);
+class DetaSessionStore extends session.Store {
+    constructor(obj) {
+        super();
+        this.deta = Deta(obj.projectKey);
+        this.db = this.deta.Base(obj.dbName);
+    }
+
+    static create(obj) {
+        return new DetaSessionStore(obj);
+    }
+
+    async set(sid, session, callback) {
+        const newSession = {
+            key: sid,
+            session: JSON.stringify(session)
         }
-
-        static create(obj){
-            return new DetaSessionStore(obj);
-        }
-
-        async set(sid,session,callback){
-            const newSession = {
-                key : sid,
-                session: JSON.stringify(session)
-            }
-            try{
-                const insertedSession = await this.db.put(newSession);
-                callback();
-            }catch(err){
-                callback(err);
-            }
-        }   
-
-        async get(sid,callback){
-            try{
-                const querySession = (await this.db.fetch({key: sid}));
-                if(querySession.count !== 0){
-                    const session = JSON.parse(querySession.items[0].session);
-
-                    callback(null, session);
-                }
-                else{
-                    callback(undefined, undefined);
-                }
-            }catch(err){
-                if(err.code === "ENONET"){
-                    callback(null,null);
-                }
-                else{
-                    callback(err,undefined);
-                }
-
-            }
-        }   
-
-        async destroy(sid,callback){
-            try{
-                const deleteStatus = await this.db.delete(String(sid));
-                callback(undefined);
-            }catch(err){
-                callback(err);
-            }
+        try {
+            const insertedSession = await this.db.put(newSession, undefined, { expireIn: 3600 }); //will expire sessions in 1 hour
+            callback();
+        } catch (err) {
+            callback(err);
         }
     }
+
+    async get(sid, callback) {
+        try {
+            const querySession = (await this.db.fetch({ key: sid }));
+            if (querySession.count !== 0) {
+                const session = JSON.parse(querySession.items[0].session);
+
+                callback(null, session);
+            }
+            else {
+                callback(undefined, undefined);
+            }
+        } catch (err) {
+            if (err.code === "ENONET") {
+                callback(null, null);
+            }
+            else {
+                callback(err, undefined);
+            }
+
+        }
+    }
+
+    async destroy(sid, callback) {
+        try {
+            const deleteStatus = await this.db.delete(String(sid));
+            callback(undefined);
+        } catch (err) {
+            callback(err);
+        }
+    }
+}
 
 module.exports = DetaSessionStore;
